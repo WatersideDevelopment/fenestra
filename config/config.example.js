935ddadd567486a2f853e2bf8c13763fb0b7a389
D. Rimron-Soutter <d@occa-design.com>
2014-04-04 14:26:50 +0100

    New example config file, removed old settings

--- config/config.example.js
+++ config/config.example.js
@@ -1,15 +1,5 @@
 module.exports = config = {
-    "name" : "Hummingbird",
-
-    // Replay some existing traffic logs to get an idea of what Hummingbird
-    // looks like.  Change this to false when you're ready to actually use
-    // Hummingbird in production.
-    "demo_mode": true,
-
-    // Port where the dashboard will be shown.  Change it to false to disable
-    // the dashboard. (you might do this if you were integrating
-    // Hummingbird into an existing admin interface)
-    "dashboard_port" : 8080,
+    "name" : "Fenestra",
 
     // If you want to have the tracking pixel listen on a different port
     // (for instance in order to password-protect your dashboard) you can
