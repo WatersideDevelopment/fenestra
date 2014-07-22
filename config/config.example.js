c62fc3c1c48aef022a9330b84d9692bc7d563950
D. Rimron-Soutter <d@occa-design.com>
2014-07-22 10:39:32 +0100

    Adding dashboard hooks
    -Dx

--- config/config.example.js
+++ config/config.example.js
@@ -4,15 +4,21 @@ module.exports = config = {
     // If you want to have the tracking pixel listen on a different port
     // (for instance in order to password-protect your dashboard) you can
     // specify the port to listen on (change from false to port number)
-    "tracking_port" : 8000,
+    "tracking_port" : 1337,
 
     // Allow stats to be sent over UDP instead of HTTP.  This works best for
     // sending stats from backend servers within the same datacenter as
-    // Hummingbird.  Change to false to disable.
-    "udp_tracking_port" : 8000,
+    // Fenestra.  Change to false to disable.
+    "udp_tracking_port" : 1337,
 
     // Interface to bind the UDP listener to. Use 127.0.0.1 to only allow
     // other apps on your machine to connect, or 0.0.0.0 to bind to all
     // interfaces and allow any machine to connect.
-    "udp_trackin_address" : "127.0.0.1"
+    "udp_trackin_address" : "127.0.0.1",
+
+	// Should I start the static server (dashboard, and client interface scripts)
+	"dashboard": true
+
+	// What dedicated port number does the dashboard live upon?
+	"dashboard_port": 1338;
 }
