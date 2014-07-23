934726ba6922966acac98015a22c0f62feef708f
D. Rimron-Soutter <d@occa-design.com>
2014-07-23 15:37:24 +0100

    DB setup script...
    -Dx

---
+++ models/database/setup.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var Promise = require('bluebird'),
+    FenestraBookshelf = require('../shelves/base');
+
+// Knex Instance
+var knex = FenestraBookshelf.knex;
+
+// Table Creation Promises
+var tablePromises = [];
+
+// Chapters Table
+tablePromises.push(new Promise(function (resolve) {
+    knex.schema.hasTable('event').then(function (exists) {
+        if (exists) {
+            console.log('Events Table Already Exists');
+            resolve();
+        } else {
+            knex.schema.createTable('event', function (t) {
+                t.increments('id');
+                t.string('type');
+                t.string('action');
+                t.string('payload');
+                t.timestamps();
+            }).then(function () {
+                console.log('Events Table Created');
+                resolve();
+            });
+        }
+    });
+}));
+
+tablePromises.push(knex.migrate.latest(
+    {directory: __dirname + '/migrations'}
+));
+
+// All Done
+Promise.all(tablePromises).then(function () {
+    // Report
+    console.log('Tables Created Or Checked...');
+    console.log('All done!');
+    process.exit();
+});
